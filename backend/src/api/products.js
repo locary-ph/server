1f4ef6e62ad78953dfdd2b9f6cf0db2812efee46
rafael <dragonwood0@protonmail.com>
2021-06-21 21:54:41 +0800

    Initial /products endpoint

--- backend/src/api/products.js
+++ backend/src/api/products.js
@@ -1,15 +1,34 @@
-const express = require('express');
-const products = require("../data/products");
+const express = require("express");
+const Product = require("../models/product");
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.json(products);
+// @desc  Fetch all products of a merchant
+// @route GET /api/v1/products
+router.get("/", async (req, res, next) => {
+  try {
+    const products = await Product.find({});
+
+    res.json(products);
+  } catch (err) {
+    next(err);
+  }
 });
 
+// @desc  Fetch single product
+// @route GET /api/v1/products/:id
+router.get("/:id", async (req, res, next) => {
+  try {
+    const product = await Product.findById(req.params.id);
 
-router.get("/:id", (req, res) => {
-  res.json(products.filter(p => p._id === req.params.id));
-})
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404).json({ message: "Product not found" });
+    }
+  } catch (err) {
+    next(err);
+  }
+});
 
 module.exports = router;
