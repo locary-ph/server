b1411ef5b50564214f398e6f788decad80ed93ab
rafael <dragonwood0@protonmail.com>
2021-06-25 12:24:41 +0800

    Added assets for styling

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import "assets/vendor/nucleo/css/nucleo.css";
+import "assets/vendor/@fortawesome/fontawesome-free/css/all.min.css";
+import "assets/css/argon-dashboard-react.css";
 
 ReactDOM.render(
   <React.StrictMode>
