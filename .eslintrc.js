e8854f0d3069b0dcbad4c2f2731241225ca845fe
rafael <dragonwood0@protonmail.com>
2021-07-02 12:40:24 +0800

    added object rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,5 @@
 module.exports = {
-  env: {
-    jest: true,
-  },
+  env: { jest: true, },
   extends: "airbnb-base",
   rules: {
     "comma-dangle": 0,
@@ -10,5 +8,10 @@ module.exports = {
     "no-return-assign": 0,
     quotes: ["error", "double"],
     camelcase: 0,
+    "object-curly-newline": ["error", {
+      multiline: true,
+      minProperties: 4
+    }],
+    "object-property-newline": ["error", { allowAllPropertiesOnSameLine: false }]
   }
 };
