7402c0ab1867607aeecebad49a04429bdf97c4dd
rafael <dragonwood0@protonmail.com>
2021-07-13 16:35:04 +0800

    Add TODO(#1)

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -1,5 +1,7 @@
 const Order = require("../models/order");
 
+// TODO(#1): Write tests for order controller
+
 // @desc Create new order
 // @route POST /api/v1/orders
 async function createOrder(req, res, next) {
@@ -19,31 +21,43 @@ async function createOrder(req, res, next) {
     const order = new Order({
       merchantId: req.user._id,
       buyer,
-      items,
+      items, // TODO: refactor to only include relevant fields
       deliveryOption,
       orderAmount,
       quantity,
       deliveryAddress,
     });
 
-    const createOrder = await order.save();
+    const createdOrder = await order.save();
 
     res.status(201);
-    res.json(createdOrder);
+    // res.json(createdOrder);
+    res.json({ message: "Order placed" });
   }
 }
 
 // @desc Fetch all orders of a merchant
 // @route GET /api/v1/orders
 async function getOrders(req, res, next) {
-  const orders = await Orders.find({ merchantId: req.user._id });
+  const orders = await Order.find({ merchantId: req.user._id });
+
+  res.json(orders);
+}
+
+// @desc Fetch 5 most recent orders
+// @route GET /api/v1/orders/recent
+async function getRecentOrders(req, res, next) {
+  const orders = await Order.find(
+    { merchantId: req.user._id },
+    "buyer.firstName buyer.lastName items.product orderAmount orderStatus"
+  );
 
   res.json(orders);
 }
 
 // @desc Fetch a single order
 // @route GET /api/v1/orders/:id
-async function getOrders(req, res, next) {
+async function getOrderById(req, res, next) {
   const order = await Order.findById(req.params.id);
 
   if (order) {
@@ -56,5 +70,7 @@ async function getOrders(req, res, next) {
 
 module.exports = {
   createOrder,
-  getOrders
+  getOrders,
+  getOrderById,
+  getRecentOrders
 };
