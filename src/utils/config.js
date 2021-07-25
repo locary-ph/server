c1f0cb7dfe78073a1c388cb6ef0bc240df7d3863
rafael <dragonwood0@protonmail.com>
2021-07-25 16:40:15 +0800

    config: add imagekit config

--- src/utils/config.js
+++ src/utils/config.js
@@ -4,7 +4,14 @@ const { PORT } = process.env;
 
 const MONGODB_URI = process.env.NODE_ENV === "test" ? process.env.TEST_MONGODB_URI : process.env.MONGODB_URI;
 
+const { IMAGEKIT_URL_ENDPOINT } = process.env;
+const { IMAGEKIT_PUBLIC_KEY } = process.env;
+const { IMAGEKIT_PRIVATE_KEY } = process.env;
+
 module.exports = {
   MONGODB_URI,
-  PORT
+  PORT,
+  IMAGEKIT_URL_ENDPOINT,
+  IMAGEKIT_PUBLIC_KEY,
+  IMAGEKIT_PRIVATE_KEY
 };
