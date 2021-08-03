9d2f27a1dd98073e5348f548c269ca0d72905973
rafael <dragonwood0@protonmail.com>
2021-08-03 17:55:16 +0800

    further changes

--- src/api/products.js
+++ src/api/products.js
@@ -5,9 +5,9 @@ const router = express.Router();
 const middlewares = require("../middlewares");
 const productController = require("../controllers/productController");
 
-router.get("/", productController.getProducts);
 router.get("/:id", productController.getProductById);
 router.use(middlewares.authorize);
+router.get("/", productController.getProducts);
 router.post("/", productController.createProduct);
 
 module.exports = router;
