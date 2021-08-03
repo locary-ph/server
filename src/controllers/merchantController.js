dc5e3f975419d5cf50ef6719723e49e521746997
rafael <dragonwood0@protonmail.com>
2021-08-03 17:34:49 +0800

    added validation if no merchant can't be found

--- src/controllers/merchantController.js
+++ src/controllers/merchantController.js
@@ -1,14 +1,21 @@
 const Merchant = require("../models/merchant");
-const Products = require("../models/product");
+const Product = require("../models/product");
 
 async function getShop(req, res) {
   if (req.query.route) {
     const { route } = req.query;
     const merchant = await Merchant.findOne({ shopUrl: route });
-    res.json(merchant);
+
+    if (merchant) {
+      const products = await Product.find({ merchantId: merchant._id });
+      res.json([merchant, products]);
+    } else {
+      res.status(404);
+      throw new Error("No merchant found");
+    }
   } else {
     res.status(400);
-    throw new Error("No `route` query parameter found");
+    throw new Error("Expected `route` parameter, received none");
   }
 }
 
