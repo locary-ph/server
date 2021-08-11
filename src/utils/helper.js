e56b9acd60bfcbf97b74fa41f1ece0731f789e29
rafael <dragonwood0@protonmail.com>
2021-08-11 11:45:18 +0800

    add helper function to check document

---
+++ src/utils/helper.js
@@ -0,0 +1,12 @@
+// @param query - returned query by mongoose
+// @param object - object to return if query is truthy
+function checkDocument(query, object) {
+  if (query) {
+    res.json(object);
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+}
+
+module.exports = { checkDocument };
