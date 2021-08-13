385c314126f775c4286ede99e105d8edec3647c2
rafael <dragonwood0@protonmail.com>
2021-08-13 11:09:49 +0800

    bind PORT env variable

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const app = require("./app");
-const config = require("./utils/config");
 
-const port = config.PORT || 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   /* eslint-disable no-console */
   console.log(`Listening: http://localhost:${port}`);
--- src/utils/config.js
+++ src/utils/config.js
@@ -1,7 +1,5 @@
 require("dotenv").config();
 
-const { PORT } = process.env;
-
 const MONGODB_URI = process.env.NODE_ENV === "test" ? process.env.TEST_MONGODB_URI : process.env.MONGODB_URI;
 
 const { IMAGEKIT_URL_ENDPOINT } = process.env;
@@ -10,7 +8,6 @@ const { IMAGEKIT_PRIVATE_KEY } = process.env;
 
 module.exports = {
   MONGODB_URI,
-  PORT,
   IMAGEKIT_URL_ENDPOINT,
   IMAGEKIT_PUBLIC_KEY,
   IMAGEKIT_PRIVATE_KEY
