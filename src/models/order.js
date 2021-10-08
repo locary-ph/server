9ce53a9e08c57c635aa98b25c8facae7299d9397
Bon Jovi Montes <60991513+BoJoNVi@users.noreply.github.com>
2021-10-08 15:22:32 +0800

    Added new values to the order details schema (#41)
    * Added new values to the order details schema
    
    * Removed the default value for the order status

--- src/models/order.js
+++ src/models/order.js
@@ -7,79 +7,90 @@ const { Schema } = mongoose;
 const BuyerSchema = new Schema({
   email: {
     type: String,
-    require: true
+    require: true,
   },
   firstName: {
     type: String,
-    require: true
+    require: true,
   },
   lastName: {
     type: String,
-    require: true
-  }
+    require: true,
+  },
 });
 
-const OrderSchema = new Schema({
-  merchantId: {
-    type: Schema.Types.ObjectId,
-    ref: "Merchant",
-    required: true
-  },
-  buyer: {
-    type: BuyerSchema,
-    required: true
-  },
-  items: [{
-    product: {
-      type: Product.schema,
-      required: true
+const OrderSchema = new Schema(
+  {
+    merchantId: {
+      type: Schema.Types.ObjectId,
+      ref: "Merchant",
+      required: true,
     },
-    quantity: {
-      type: Number,
-      required: true
-    }
-  }],
-  deliveryOption: {
-    type: String,
-    enum: ["Pick up", "Delivery"]
-  },
-  orderAmount: {
-    type: String,
-    required: true
-  },
-  quantity: Number,
-  deliveryAddress: {
-    line1: {
-      type: String,
-      required: true
+    buyer: {
+      type: BuyerSchema,
+      required: true,
     },
-    line2: String,
-    city: {
-      type: String,
-      required: true
-    },
-    province: {
-      type: String,
-      required: true
-    },
-    zipcode: String
-  },
-  paymentStatus: {
-    type: String,
-    enum: ["Pending", "Paid"],
-    default: "Pending",
-  },
-  orderStatus: {
-    type: String,
-    enum: [
-      "PENDING",
-      "ACCEPTED",
-      "DISPATCHED",
-      "DELIVERED",
-      "CANCELLED"
+    items: [
+      {
+        product: {
+          type: Product.schema,
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+        },
+      },
     ],
-    default: "PENDING"
-  }
-}, { timestamps: true });
+    deliveryOption: {
+      type: String,
+      enum: ["PICKUP", "DELIVERY"],
+    },
+    paymentOption: {
+      type: String,
+      enum: ["EWALLET", "BANK", "PICKUP", "DELIVERY"],
+    },
+    orderAmount: {
+      type: String,
+      required: true,
+    },
+    quantity: Number,
+    deliveryAddress: {
+      line1: {
+        type: String,
+        required: true,
+      },
+      line2: String,
+      city: {
+        type: String,
+        required: true,
+      },
+      province: {
+        type: String,
+        required: true,
+      },
+      zipcode: String,
+    },
+    paymentStatus: {
+      type: String,
+      enum: ["Pending", "Paid"],
+      default: "Pending",
+    },
+    orderStatus: {
+      type: String,
+      enum: [
+        "PAID",
+        "ORDER PLACED",
+        "APPROVED",
+        "REJECT",
+        "TO DELIVER",
+        "ON THE WAY",
+        "DELIVERED",
+        "CANCELLED",
+      ],
+    },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Order", OrderSchema);
